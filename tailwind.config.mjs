9dcd13edebca4e726bdf99b032e5b6b97e2445a2
aahda <angel.huaman0528@gmail.com>
2024-07-25 21:18:30 -0500

    Galería del módulo nosotros.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        agrandarPrecio: {
+          "0%": { fontSize: "150%" },
+          "50%": { fontSize: "180%" },
+          "100%": { fontSize: "150%" },
+        },
+      },
+      animation: {
+        agrandarPrecio: "agrandarPrecio 3s infinite ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
